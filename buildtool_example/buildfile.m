4b2dceb69226c412c582ac2ae90d2500a883d8fb
Vahila <70003902+Vahila@users.noreply.github.com>
2023-04-03 16:51:59 +0530

    Create buildfile.m

---
+++ buildtool_example/buildfile.m
@@ -0,0 +1,30 @@
+function plan = buildfile
+% Create a plan from the task functions
+plan = buildplan(localfunctions);
+
+% Make the "archive" task the default task in the plan
+plan.DefaultTasks = "archive";
+
+% Make the "archive" task dependent on the "check" and "test" tasks
+plan("archive").Dependencies = ["check" "test"];
+end
+
+function checkTask(~)
+% Identify code issues
+issues = codeIssues;
+assert(isempty(issues.Issues),formattedDisplayText( ...
+    issues.Issues(:,["Location" "Severity" "Description"])))
+end
+
+function testTask(~)
+% Run unit tests
+results = runtests(IncludeSubfolders=true,OutputDetail="terse");
+assertSuccess(results);
+end
+
+function archiveTask(~)
+% Create ZIP file
+zipFileName = "source_" + ...
+    string(datetime("now",Format="yyyyMMdd'T'HHmmss"));
+zip(zipFileName,"*")
+end
