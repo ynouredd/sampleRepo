2dfd7f8e1da6311874c742344bf2a1c32f4a533e
Vahila <70003902+Vahila@users.noreply.github.com>
2023-04-03 16:06:41 +0530

    Create testMul.m

---
+++ test/testFolder/testMul.m
@@ -0,0 +1,13 @@
+classdef testCalc < matlab.unittest.TestCase
+    %TESTCALC Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    methods(Test)
+        
+        function testMultiplication(testCase)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            testCase.verifyEqual(mulNum(2,2), 4);
+        end
+    end
+end
