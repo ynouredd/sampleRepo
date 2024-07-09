3e66039a8edc90223a4f8622b03301b76996f784
Vahila <70003902+Vahila@users.noreply.github.com>
2024-07-09 09:36:25 +0530

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -1,44 +1,33 @@
 function plan = buildfile
- 
+% Create a plan from the task functions
 plan = buildplan(localfunctions);
-plan.DefaultTasks = "my";
- 
+
+% Make the "archive" task the default task in the plan
+plan.DefaultTasks = "archive";
+
+% Make the "archive" task dependent on the "check" and "test" tasks
+plan("archive").Dependencies = ["check" "test"];
 end
- 
-function myTask(~)
-    disp('Hello World!')
-    error('my error')
+
+function checkTask(~)
+% Identify code issues
+% issues = codeIssues;
+% assert(isempty(issues.Issues),formattedDisplayText( ...
+%    issues.Issues(:,["Location" "Severity" "Description"])))
+disp('In check task');
 end
-% function plan = buildfile
-% % Create a plan from the task functions
-% plan = buildplan(localfunctions);
-% 
-% % Make the "archive" task the default task in the plan
-% plan.DefaultTasks = "archive";
-% 
-% % Make the "archive" task dependent on the "check" and "test" tasks
-% plan("archive").Dependencies = ["check" "test"];
-% end
-% 
-% function checkTask(~)
-% % Identify code issues
-% % issues = codeIssues;
-% % assert(isempty(issues.Issues),formattedDisplayText( ...
-% %    issues.Issues(:,["Location" "Severity" "Description"])))
-% disp('In check task');
-% end
-% 
-% function testTask(~)
-% % Run unit tests
-% % results = runtests(IncludeSubfolders=true,OutputDetail="terse");
-% % assertSuccess(results);
-% disp('In test task');
-% end
-% 
-% function archiveTask(~)
-% % Create ZIP file
-% % zipFileName = "source_" + ...
-% %     string(datetime("now",Format="yyyyMMdd'T'HHmmss"));
-% % zip(zipFileName,"*")
-% disp('In archive task');
-% end
+
+function testTask(~)
+% Run unit tests
+% results = runtests(IncludeSubfolders=true,OutputDetail="terse");
+% assertSuccess(results);
+disp('In test task');
+end
+
+function archiveTask(~)
+% Create ZIP file
+% zipFileName = "source_" + ...
+%     string(datetime("now",Format="yyyyMMdd'T'HHmmss"));
+% zip(zipFileName,"*")
+disp('In archive task');
+end
